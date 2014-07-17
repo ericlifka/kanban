2217bbf1b2956828ca00f278dbe09df50d973647
Eric Lifka <eric.lifka@gmail.com>
2014-07-16 20:44:27 -0400

    Install bootstrap

--- ui/Brocfile.js
+++ ui/Brocfile.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
